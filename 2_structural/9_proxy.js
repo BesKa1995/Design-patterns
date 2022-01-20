1c1f69da310b15ed78523f3a1990206931a6392b
Beso <besopoladashvili@gmail.com>
2022-01-20 08:11:33 -0800

    design patter proxy added

---
+++ 2_structural/9_proxy.js
@@ -0,0 +1,23 @@
+//fetch optimization
+
+function networkFetch(url) {
+	return `${url} - response from server`
+}
+
+const cache = []
+
+const proxiedFetch = new Proxy(networkFetch, {
+	apply(target, thisArg, args) {
+		const url = args[0]
+		if (cache.includes(url)) {
+			return `${url} - response from cache`
+		} else {
+			cache.push(url)
+			return target(args[0])
+		}
+	}
+})
+
+console.log(proxiedFetch('google.com'))
+console.log(proxiedFetch('google.com'))
+console.log(proxiedFetch('facebook.com'))
