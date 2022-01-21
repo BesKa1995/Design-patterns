430f6eb728bd477f0d5a06541d200f2d67ed09ab
Beso <besopoladashvili@gmail.com>
2022-01-21 10:05:24 -0800

    desing patter iterator added

---
+++ 3_behaviour/12_iterator.js
@@ -0,0 +1,33 @@
+class MyIterator {
+	constructor(data = []) {
+		this.index = 0
+		this.data = data
+
+	}
+
+	[Symbol.iterator]() {
+		return {
+			next: () => {
+				if (this.index < this.data.length) {
+					return {
+						value: this.data[this.index++],
+						done: false
+					}
+				} else {
+					this.index = 0
+					return {
+						value: undefined,
+						done: true
+					}
+				}
+			}
+		}
+	}
+}
+
+
+const iterator = new MyIterator(['this', 'is', 'iterator'])
+
+for (const iteratorElement of iterator) {
+	console.log(iteratorElement)
+}
